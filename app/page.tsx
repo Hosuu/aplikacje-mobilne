40e6500b1e32cc4fa96d9519394274843b8c74c4
Mateusz Siller <mateusz.siller@gmail.com>
2024-05-20 00:14:38 +0200

    double commit is my passion :nerd:

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation"
 
 export default async function Home() {
   const session = await auth()
-  console.log(session)
   if (session != undefined) redirect("/app")
 
   return (
@@ -12,20 +11,9 @@ export default async function Home() {
       <h1 className='text-4xl font-bold'>
         WroKawka <p className='text-sm'>Coś wiecej, niż tylko kawiarnie</p>
       </h1>
-      {session === null ? (
-        <Link href={"/login"} className='bg-sky-600 hover:bg-sky-800  rounded-full text-center py-2 px-5'>
-          Zaloguj
-        </Link>
-      ) : (
-        <div className='flex flex-col gap-2'>
-          <p>
-            Zalogowano jako: <em>{session?.user?.email}</em>
-          </p>
-          <Link href={"/logout"} className='bg-sky-600 hover:bg-sky-800 rounded-full text-center py-2 px-5'>
-            Wyloguj
-          </Link>
-        </div>
-      )}
+      <Link href={"/login"} className='bg-sky-600 hover:bg-sky-800  rounded-full text-center py-2 px-5'>
+        Zaloguj
+      </Link>
     </main>
   )
 }
